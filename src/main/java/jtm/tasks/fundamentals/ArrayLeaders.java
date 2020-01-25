e21b670bc0923cfe73d7e5392fca07bddf924784
Catttiebri <ssantucis@gmail.com>
2020-01-25 11:17:13 +0200

    Array Leaders - DONE!

--- src/main/java/jtm/tasks/fundamentals/ArrayLeaders.java
+++ src/main/java/jtm/tasks/fundamentals/ArrayLeaders.java
@@ -1,5 +1,6 @@
 package jtm.tasks.fundamentals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class ArrayLeaders {
@@ -16,7 +17,30 @@ public class ArrayLeaders {
      */
     public static int[] arrayLeaders(int[] numbers) {
 
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        int size = numbers.length;
+        for (int i = 0; i < size; i++) {
+            int afterSize = size - i - 1;
+            int[] after = new int[afterSize];
+            for (int k = 0; k < afterSize; k++) {
+                after[k] = numbers[i + 1 + k];
+            }
+            if (numbers[i] > sum(after)) {
+                answer.add(numbers[i]);
+            }
+        }
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = answer.get(i).intValue();
+        }
+        return result;
+    }
 
-        return null;
+    public static int sum(int[] array) {
+        int answer = 0;
+        for (int number : array) {
+            answer += number;
+        }
+        return answer;
     }
 }
