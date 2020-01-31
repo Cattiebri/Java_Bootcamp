e59f0755dfe8c587e437bbabf0475035751778d8
Catttiebri <ssantucis@gmail.com>
2020-01-31 18:57:16 +0200

    Extra03 still in progress (3101)

--- src/main/java/jtm/extra03/PracticalNumbers.java
+++ src/main/java/jtm/extra03/PracticalNumbers.java
@@ -9,17 +9,19 @@ public class PracticalNumbers {
     // TODO Read article https://en.wikipedia.org/wiki/Practical_number
     // Implement method, which returns practical numbers in given range
     // including
-    public String getPracticalNumbers(int from, int to){;
+    public String getPracticalNumbers(int from, int to) {
+
         String answer = "[";
         int[] numbers = fillArray(from, to);
-		System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers));
         for (int i = 0; i < numbers.length; i++) {
-        	if (findDivisors(numbers[i])) {
-				answer = answer + numbers[i];
-				if (!(i + 1 == numbers.length)) {
-					answer = answer + ", ";
-				}
-			}
+            if (findDivisors(numbers[i])) {
+                if(answer.equals("[")){
+                    answer = answer + numbers[i];
+                } else {
+                    answer = answer + ", " + numbers[i];
+                }
+            }
         }
         answer = answer + "]";
         return answer;
@@ -38,17 +40,36 @@ public class PracticalNumbers {
     }
 
     private boolean findDivisors(int number) {
-		ArrayList<Integer> myList = new ArrayList<>();
+        ArrayList<Integer> myList = new ArrayList<>();
 
-		ArrayList<Integer> falseList = new ArrayList<>();
-    	int[] test = fillArray(1, number);
-        for (int i = 0; i < test.length; i++) {
+        ArrayList<Integer> falseList = new ArrayList<>();
+        int[] test = fillArray(1, number);
+        for (int i = 1; i < test.length; i++) {
             if (number % i == 0) {
-				myList.add(i);
+                myList.add(i);
             } else {
-            	falseList.add(i);
-			}
+                falseList.add(i);
+            }
         }
-	return true;
+        int sum = myList.get(0);
+        boolean status = false;
+//        for (int i = 0; i < myList.size(); i++) {
+            for (int j = 1; j < myList.size() - 1; j++) {
+                if (sum < myList.get(j) - 1) {
+                    status = false;
+                    break;
+                } else {
+                    status = true;
+                    sum += myList.get(j);
+                }
+            }
+//            if (status && sum == myList.get(i)) {
+//                status = true;
+//                return status;
+//            } else {
+//                return false;
+//            }
+//        }
+    return status;
     }
 }
