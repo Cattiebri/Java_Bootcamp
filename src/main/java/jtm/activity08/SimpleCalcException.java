d5ef524ed62422db6fb47b49fb627e167a2130c2
Catttiebri <ssantucis@gmail.com>
2020-02-10 20:43:17 +0200

    Try-Catch task - DONE!

---
+++ src/main/java/jtm/activity08/SimpleCalcException.java
@@ -0,0 +1,12 @@
+package jtm.activity08;
+
+public class SimpleCalcException extends Exception {
+    public SimpleCalcException(String s) {
+        super(s);
+    }
+
+    public SimpleCalcException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+}
--- src/main/java/jtm/extra03/PracticalNumbers.java
+++ src/main/java/jtm/extra03/PracticalNumbers.java
@@ -16,7 +16,7 @@ public class PracticalNumbers {
         System.out.println(Arrays.toString(numbers));
         for (int i = 0; i < numbers.length; i++) {
             if (findDivisors(numbers[i])) {
-                if(answer.equals("[")){
+                if (answer.equals("[")) {
                     answer = answer + numbers[i];
                 } else {
                     answer = answer + ", " + numbers[i];
@@ -40,28 +40,61 @@ public class PracticalNumbers {
     }
 
     private boolean findDivisors(int number) {
-        ArrayList<Integer> myList = new ArrayList<>();
+        ArrayList<Integer> divisorList = new ArrayList<>();
 
-        ArrayList<Integer> falseList = new ArrayList<>();
         int[] test = fillArray(1, number);
-        for (int i = 1; i < test.length; i++) {
-            if (number % i == 0) {
-                myList.add(i);
-            } else {
-                falseList.add(i);
+        for (int i = 0; i < test.length; i++) {
+            if (number % (i+1) == 0) {
+                divisorList.add(test[i]);
+            }
+            else {
+                break;
             }
         }
-        int sum = myList.get(0);
         boolean status = false;
-            for (int j = 1; j < myList.size() - 1; j++) {
-                if (sum < myList.get(j) - 1) {
-                    status = false;
-                    break;
-                } else {
-                    status = true;
-                    sum += myList.get(j);
-                }
+        int sum = divisorList.get(0);
+        for (int i = 1; i < divisorList.size() - 1; i++) {
+            if (sum < divisorList.get(i) - 1) {
+                status = false;
+                break;
+            } else {
+                status = true;
+                sum += divisorList.get(i);
             }
-    return status;
+
+        }
+        return status;
+//        check(divisorList,number);
+//    }
+
+//    boolean status = false;
+//
+//    //        for (int j = 0; j < divisorList.size(); j++) {
+////            if (sum < divisorList.get(j)) {
+////                status = false;
+////                return status;
+////            } else {
+////                status = true;
+////                sum += divisorList.get(j);
+////            }
+////        }
+//    private void check(ArrayList<Integer> arr, int number){
+//    int []ispossible = new int[number];
+//
+//        for (int i = 0; i < number; ++i) {
+//            int val = arr.get(i);
+//
+//            // if it is already possible
+//            if (ispossible[val] == arr.get(i)) {
+//                continue;
+//            }
+//
+////            // make 1 to all it's next elements
+////            for (int j = 0; j + val < 1000; ++j)
+////                if (ispossible[j] == 1)
+////                    ispossible[j + val] = 1;
+//        }
+//        return
+//    }
     }
 }
