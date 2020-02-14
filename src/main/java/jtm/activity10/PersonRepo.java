2cb42c7cb10531980d69b41ddd797e8154db8017
Catttiebri <ssantucis@gmail.com>
2020-02-14 21:31:28 +0200

    Activity 10 - DONE!

--- src/main/java/jtm/activity10/PersonRepo.java
+++ src/main/java/jtm/activity10/PersonRepo.java
@@ -10,13 +10,14 @@ TODO Necessary imports commented
 //import java.nio.file.Paths;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jtm.activity09.Order;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class PersonRepo {
+public class PersonRepo implements Comparator<Person> {
 
     private List<Person> personList;
 
@@ -34,10 +35,9 @@ public class PersonRepo {
             Path path = Paths.get("src/main/resources/data.json");
             List<String> data = Files.readAllLines(path);
             String listString = String.join("\n ", data);
-            System.out.println(listString);
             ObjectMapper mapper = new ObjectMapper();
             Person[] personArray = mapper.readValue(listString, Person[].class);
-            List<Person> personList = Arrays.asList(personArray);
+            this.personList = Arrays.asList(personArray);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -46,27 +46,48 @@ public class PersonRepo {
 
     public Person oldestPerson() {
         //Find oldest person in personList field and return it
-//        Collections.sort(Person., new Comparator<Score>() {
-//
-//            public int compare(Score o1, Score o2) {
-//                // compare two instance of `Score` and return `int` as result.
-//                return o2.getScores().get(0).compareTo(o1.getScores().get(0));
-//            }
-//        });
-        return null;
+        Person oldest = personList.get(0);
+        for (int i = 0; i < personList.size() - 1; i++) {
+            if (compare(oldest, personList.get(i + 1)) >= 0) {
+                oldest = personList.get(i + 1);
+            }
+        }
+        return oldest;
     }
 
     public Person youngestPerson() {
         //Find youngest person in personList field and return it
-        return null;
+        Person youngest = personList.get(0);
+        for (int i = 0; i < personList.size() - 1; i++) {
+            if (compare(youngest, personList.get(i + 1)) <= 0) {
+                youngest = personList.get(i + 1);
+            }
+        }
+        return youngest;
     }
 
     public String largestPopulation() {
         //Find country with largest population and return it's name
-        return null;
+        Map<String, Integer> country = new HashMap<>();
+        int temp = 0;
+        String middle = "";
+        for (int i = 0; i < personList.size(); i++) {
+            temp = 0;
+            middle = personList.get(i).getCountry();
+            if (country.containsKey(middle)) {
+                temp = country.get(middle);
+                country.replace(middle, ++temp);
+            } else {
+                country.put(personList.get(i).getCountry(), 1);
+            }
+        }
+        return Collections.max(country.entrySet(), Comparator.comparingInt(Map.Entry::getValue)).getKey();
     }
 
-
+    @Override
+    public int compare(Person person, Person t1) {
+        return person.getBirthDate().compareTo(t1.getBirthDate());
+    }
 }
 
 
