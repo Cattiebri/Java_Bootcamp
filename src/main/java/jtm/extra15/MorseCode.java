26919f0d2989125a53e23d6570e72bea6989c857
Catttiebri <ssantucis@gmail.com>
2020-02-21 21:27:08 +0200

    extra15 - in progress

--- src/main/java/jtm/extra15/MorseCode.java
+++ src/main/java/jtm/extra15/MorseCode.java
@@ -1,7 +1,9 @@
 package jtm.extra15;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,11 +25,36 @@ public class MorseCode {
             Note that morse code does not have capitalization e.g. T and t are both the same in morse code: '-'
 
      */
-    public static String translateToMorse(String text) {
-        return null;
+    public static String translateToMorse(String text) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader("src/main/java/jtm/extra15/MorseLetters.txt"));
+        String str;
+
+        List<String> list = new ArrayList<>();
+        while((str = in.readLine()) != null){
+            list.add(str);
+        }
+
+        String[] stringArr = list.toArray(new String[0]);
+
+        int textLength = text.length();
+        String[] textArray = text.split("(?!^)");
+
+        String answer = "";
+
+        for(int k = 0; k<textLength; k++) {
+            for (int i = 0; i < stringArr.length; i++) {
+                if (stringArr[i].contains(textArray[k])) {
+                    answer = answer + stringArr[i].substring(3) + " ";
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(textArray));
+
+        return answer;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String expected = "- .... .- -. -.- | -.-- --- ..- | ...- . .-. -.-- | -- ..- -.-. ....";
         String result = translateToMorse("Thank you very much");
 
