275834eda20d85bcdec884cd6ff3d1905ec3fd35
Catttiebri <ssantucis@gmail.com>
2020-02-22 14:35:46 +0200

    Lunar Calculator - done!

--- src/main/java/jtm/extra16/LunarCalculator.java
+++ src/main/java/jtm/extra16/LunarCalculator.java
@@ -32,10 +32,78 @@ public class LunarCalculator {
 
     public static long add(long number1, long number2) {
         /* The moon people believe in you. */
-        return -1;
+        String n1string = String.valueOf(number1);
+        String[] n1array = n1string.split("(?<=.)");
+
+        String n2string = String.valueOf(number2);
+        String[] n2array = n2string.split("(?<=.)");
+
+        String[] answerArray;
+        String[] unchangedArray;
+        String[] changedArray;
+
+        if (n1array.length > n2array.length) {
+            answerArray = new String[n1array.length];
+            unchangedArray = new String[n1array.length];
+            for (int i = 0; i < n1array.length; i++) {
+                unchangedArray[i] = n1array[i];
+            }
+            changedArray = new String[n1array.length];
+            int forZeros = n1array.length - n2array.length;
+            int counter = 0;
+            for (int k = 0; k < n1array.length; k++) {
+                if (k < forZeros) {
+                    changedArray[k] = "0";
+                } else {
+                    changedArray[k] = n2array[counter++];
+                }
+            }
+
+            System.out.println("Unchanged array: " + Arrays.toString(unchangedArray));
+            System.out.println("Changed array: " + Arrays.toString(changedArray));
+        } else {
+            answerArray = new String[n2array.length];
+            unchangedArray = new String[n2array.length];
+            for (int i = 0; i < n2array.length; i++) {
+                unchangedArray[i] = n2array[i];
+            }
+            changedArray = new String[n2array.length];
+            int forZeros = n2array.length - n1array.length;
+            int counter = 0;
+            for (int k = 0; k < n2array.length; k++) {
+                if (k < forZeros) {
+                    changedArray[k] = "0";
+                } else {
+                    changedArray[k] = n1array[counter++];
+                }
+            }
+
+            System.out.println("Unchanged array: " + Arrays.toString(unchangedArray));
+            System.out.println("Changed array: " + Arrays.toString(changedArray));
+        }
+
+        for (int i = 0; i < answerArray.length; i++) {
+            if (Integer.parseInt(changedArray[i]) > Integer.parseInt(unchangedArray[i])) {
+                answerArray[i] = changedArray[i];
+            } else {
+                answerArray[i] = unchangedArray[i];
+            }
+        }
+        System.out.println("Array with largest values: " + Arrays.toString(answerArray));
+
+        String answer = "";
+        //for (String el : answerArray) {
+            answer = String.join("",answerArray);
+        //}
+        System.out.println(answer);
+
+        long l = Long.parseLong(answer);
+        return l;
     }
 
     public static void main(String[] args) {
+
         add(465, 72);
+        add(9, 245);
     }
 }
