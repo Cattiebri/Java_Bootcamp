e106f080842c3ba5fab5aa3a7efe5036201f9fd7
Catttiebri <ssantucis@gmail.com>
2020-02-24 19:17:26 +0200

    Poker Straight - done!

--- src/main/java/jtm/extra17/PokerStraight.java
+++ src/main/java/jtm/extra17/PokerStraight.java
@@ -1,5 +1,6 @@
 package jtm.extra17;
 
+import java.util.Collections;
 import java.util.List;
 
 /*
@@ -19,6 +20,32 @@ The number of cards will vary, but will never be more than 7 (the board (5) + pl
 public class PokerStraight {
 
     public static boolean isStraight(List<Integer> cards) {
+
+        Collections.sort(cards);
+
+        System.out.println(cards);
+        int counter = 0;
+        for (int i = 0; i < cards.size(); i++) {
+
+            if ((cards.get(cards.size() - 1) == 14) && counter == 0) {
+                counter = 1;
+            }
+            if (!(i + 1 == cards.size())) {
+                if (((cards.get(i) + 1) == cards.get(i + 1)) || ((cards.get(i) == 5 && (cards.get(i+1) == 14)))) {
+                    counter++;
+                    if (counter >= 5) {
+                        return true;
+                    }
+                }
+            } else {
+                if ((cards.get(i) == cards.get(i - 1) + 1)) {
+                    counter++;
+                    if (counter >= 5) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
